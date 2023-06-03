5b2331ac05f293ecccc7f0df6972cef5e80ded45
Rajesh Satpathy <rajesh.a.s.1997@gmail.com>
2023-06-03 15:28:49 -0500

    Add NFT minting test

--- src/backend/test/NFTMarketplace.test.js
+++ src/backend/test/NFTMarketplace.test.js
@@ -3,6 +3,7 @@ const { expect } = require("chai");
 describe("NFTMarketplace", function(){
     let deployer, addr1, addr2, nft, marketplace;
     let feePercent = 1;
+    let URI = "Sample URI";
 
     beforeEach(async function(){
         // Get contract factories
@@ -26,6 +27,22 @@ describe("NFTMarketplace", function(){
             expect(await marketplace.feeAccount()).to.equal(deployer.address);
             expect(await marketplace.feePercent()).to.equal(feePercent);
         });
-    })
+    });
+
+    describe("Minting NFTs", () => {
+        it("Should track each minted NFT", async () => {
+            // addr1 mints an NFT
+            await nft.connect(addr1).mint(URI);
+            expect(await nft.tokenCount()).to.equal(1);
+            expect(await nft.balanceOf(addr1.address)).to.equal(1);
+            expect(await nft.tokenURI(1)).equals(URI);
+
+            // addr2 mints an NFT
+            await nft.connect(addr2).mint(URI);
+            expect(await nft.tokenCount()).to.equal(2);
+            expect(await nft.balanceOf(addr2.address)).to.equal(1);
+            expect(await nft.tokenURI(2)).equals(URI);
+        });
+    });
 
 });
