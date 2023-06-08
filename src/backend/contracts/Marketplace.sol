cba52fa2f6c132eb15c6414b90b71edf6db13bc9
Rajesh Satpathy <rajesh.a.s.1997@gmail.com>
2023-06-07 19:55:50 -0500

    Add Item creation, event and test

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -10,8 +10,54 @@ contract Marketplace is ReentrancyGuard {
     uint public immutable feePercent;
     uint public itemCount;
 
+    struct Item {
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
+    }
+
+    event Offered (
+        uint ItemId,
+        address indexed nft,
+        uint tokenId,
+        uint price,
+        address indexed seller
+    );
+
+    // ItemId -> Item
+    mapping(uint => Item) public items;
+
     constructor(uint _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    // Make item to offer on the marketplace
+    function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
+        require(_price > 0, "Price must be greater than zero");
+        // increment itemCount
+        itemCount ++;
+        // transfer nft
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        // add new item to items mapping
+        items[itemCount] = Item (
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+        // emit Offered event
+        emit Offered(
+            itemCount,
+            address(_nft),
+            _tokenId,
+            _price,
+            msg.sender
+        );
     }
 }
--- src/backend/test/NFTMarketplace.test.js
+++ src/backend/test/NFTMarketplace.test.js
@@ -1,5 +1,8 @@
 const { expect } = require("chai");
 
+const toWei = (num) => ethers.utils.parseEther(num.toString())
+const fromWei = (num) => ethers.utils.formatEther(num)
+
 describe("NFTMarketplace", function(){
     let deployer, addr1, addr2, nft, marketplace;
     let feePercent = 1;
@@ -45,4 +48,43 @@ describe("NFTMarketplace", function(){
         });
     });
 
+    describe("Making Marketplace items", () => {
+        let price = 1;
+        beforeEach(async () => {
+            // addr1 mints and NFT
+            await nft.connect(addr1).mint(URI);
+            // addr1 approves marketplace to spend NFT
+            await nft.connect(addr1).setApprovalForAll(marketplace.address, true);
+        });
+
+        it("Should track newly created item, transfer NFT from seller to marketplace and emit Offered event", async () => {
+            // addr1 offers their nft at a price of 1 ether
+            await expect(marketplace.connect(addr1).makeItem(nft.address, 1 , toWei(price)))
+              .to.emit(marketplace, "Offered")
+              .withArgs(
+                1,
+                nft.address,
+                1,
+                toWei(price),
+                addr1.address
+              )
+            // Owner of NFT should now be the marketplace
+            expect(await nft.ownerOf(1)).to.equal(marketplace.address);
+            // Item count should now equal 1
+            expect(await marketplace.itemCount()).to.equal(1)
+            // Get item from items mapping then check fields to ensure they are correct
+            const item = await marketplace.items(1)
+            expect(item.itemId).to.equal(1)
+            expect(item.nft).to.equal(nft.address)
+            expect(item.tokenId).to.equal(1)
+            expect(item.price).to.equal(toWei(price))
+            expect(item.sold).to.equal(false)
+          });
+
+          it("Should fail if price is set to zero", async function () {
+            await expect(
+              marketplace.connect(addr1).makeItem(nft.address, 1, 0)
+            ).to.be.revertedWith("Price must be greater than zero");
+          });
+    });
 });
